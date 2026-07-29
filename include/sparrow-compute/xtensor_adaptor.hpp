fa267ea52180a79699f17bfdeb1775538e19e5aa
Alexis Placet <alexis.placet.dev@pm.me>
2026-07-29 10:07:00 +0200

    Revert "fix codpseed workflow"
    This reverts commit 80c59d88328f189ae32ad752792e2b0d52b7bb87.

--- include/sparrow-compute/xtensor_adaptor.hpp
+++ include/sparrow-compute/xtensor_adaptor.hpp
@@ -17,15 +17,6 @@ namespace sparrow::compute
 {
     namespace detail
     {
-        template <class T>
-        concept supports_uninitialized_u8_buffer = requires {
-            typename sparrow::u8_buffer<T>::uninitialized_t;
-            sparrow::u8_buffer<T>(
-                std::size_t{},
-                typename sparrow::u8_buffer<T>::uninitialized_t{}
-            );
-        };
-
         /// Get a raw pointer to the contiguous value buffer of a
         /// sparrow primitive array.
         template <sparrow::primitive_type T>
@@ -60,22 +51,8 @@ namespace sparrow::compute
     {
         const auto n = static_cast<std::size_t>(expr.shape()[0]);
         // xtensor overwrites every element below before ownership transfers to
-        // primitive_array. Use uninitialized output storage when the linked
-        // Sparrow version supports it; retain compatibility with older releases.
-        auto buffer = [&]
-        {
-            if constexpr (detail::supports_uninitialized_u8_buffer<T>)
-            {
-                return sparrow::u8_buffer<T>(
-                    n,
-                    typename sparrow::u8_buffer<T>::uninitialized_t{}
-                );
-            }
-            else
-            {
-                return sparrow::u8_buffer<T>(n);
-            }
-        }();
+        // primitive_array, so avoid value-initializing this output buffer.
+        sparrow::u8_buffer<T> buffer(n, sparrow::uninitialized_t{});
         auto view = xt::adapt(
             buffer.data(),
             n,
